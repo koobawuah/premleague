d4c186d20ecf91cde6ac0fdc1b6449af6114fe14
koobawuah <me@bawuahboakye.com>
2022-07-10 22:44:21 +0000

    feat: Added Footer section to the Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -20,6 +20,26 @@ const Navbar = () => {
 }
 
 
+
+const Footer= () => {
+
+    const date = new Date()
+
+    return (
+        <div className="w-full h-20 text-white mx-auto py-10">
+            <div className="flex justify-center gap-x-4 text-lg font-medium"> 
+                <Link href="/">
+                    <a className="font-black">Premleague</a>
+                </Link>
+                <p> &copy; {date.getFullYear()} All Rights Reserved.</p>
+                <Link href="https://bawuahboakye.com">
+                    <a className="text-purple-200 block"> &lt;/bawuahboakye&gt;</a>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
 const Layout = ({children}) => {
     
     return (
@@ -28,6 +48,7 @@ const Layout = ({children}) => {
             <div className="py-2">
                 {children}
             </div>
+            <Footer />
         </div>
     )
 }
