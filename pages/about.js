1f2e84263b57cbb338905931e1f4d1ea31498823
koobawuah <me@bawuahboakye.com>
2022-08-04 22:08:18 +0000

    feat: updating the pages and content

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Layout } from '../components'
 
 const About = () => {
@@ -7,9 +8,13 @@ const About = () => {
     return (
         <div>
         <Layout>
-            <div className="w-full h-screen flex flex-col justify-center items-center text-white">
-                <h2 className="font-black"> PremLeague &copy; {d.getFullYear()} </h2>
+            <div className="h-[700px] flex flex-col justify-center items-center text-white">
+                <h2 className="font-black"> FixComp &copy; {d.getFullYear()} </h2>
                 <p className="max-w-3xl text-center text-lg"> We help you analyze your fixtures to understand and experience the game we love in a beautiful way! </p>
+
+                <Link href="https://21gzzpe79j5.typeform.com/to/A2bADUFD">
+                    <a className="text-5xl text-white font-black my-40 hover:underline">Tell us what you think</a>
+                </Link>
             </div>
         </Layout>
         </div>
---
+++ pages/scoreboard.js
@@ -0,0 +1,13 @@
+import { Layout } from "../components";
+
+const Scoreboard = () => {
+  return (
+    <Layout>
+      <div className="h-[750px]">
+        <h2> Scores </h2>
+      </div>
+    </Layout>
+  );
+};
+
+export default Scoreboard;
