20a9a0a181317cb28dad58d78ce0866276ea6a58
koobawuah <me@bawuahboakye.com>
2022-08-05 13:45:57 +0000

    feat: added title, meta and Google Adsense script tags to enable ads. Added notice for v2 launching soon.

--- pages/scoreboard.js
+++ pages/scoreboard.js
@@ -1,10 +1,17 @@
+import Head from "next/head";
 import { Layout } from "../components";
 
 const Scoreboard = () => {
   return (
     <Layout>
-      <div className="h-[750px]">
-        <h2> Scores </h2>
+      <Head>
+        <title>Scoreboard - FixComp | Fixtures Analyzer </title>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3573939675911361"
+     crossOrigin="anonymous"></script>
+      </Head>
+      <div className="w-full h-[800px] flex flex-col justify-center items-center">
+        <h2 className="text-2xl text-white font-bold"> Scoreboard </h2>
+        <p className="text-lg text-white font-normal"> Launching soon - v2</p>
       </div>
     </Layout>
   );
